27f3028f7034300cbef323fa0b13021b457b5d5b
sato <matheussato1702@gmail.com>
2026-06-12 11:56:54 -0300

    Add files via upload

---
+++ pages/subs/script.js
@@ -0,0 +1,7 @@
+function resolver(){
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    document.getElementById('res').textContent = n1 - n2
+    
+
+}
